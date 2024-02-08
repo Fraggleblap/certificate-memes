0dcee0752ed35b4f9a88cd65db3313e991e41be5
Fraggleblap <Owen.Gaffney23@student.cart.org>
2024-02-08 15:49:13 -0800

    more

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
         buttons.forEach(button => button.addEventListener('click', processForm))
     }
 
+    document.getElementById('name').addEventListener('change', function (eventData) {
+        let nameOutput = document.getElementById('nameLabel');
+        // console.log(eventData);
+        nameOutput.innerText = eventData.target.value;
+    });
+
     document.getElementById('type').addEventListener('change', function (eventData) {
-        let info = document.getElementById('testOutput');
-        info.innerText = eventData.target.value;
+        console.log(eventData.target.value);
         changeCert(eventData.target.value);
     });
 
@@ -21,28 +26,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
         //update certificate information when form is submitted:
         let info = document.getElementById('type');
-        nameOutput.innerText = formData.name;
         info.innerText = formData.type;
 
     });
     // Log readiness to console
     console.log("Ready");
 
+    function getDate() {
+        let theDate = new Date().toLocaleDateString('en-us', {weekday:"long", year:"numeric", month:"short", day:"numeric"})
+        //let dateBox = document.getElementById('theDate');
+        //dateBox.innerText = theDate;
+        return theDate;
+    }
+
     function changeCert(value) {
-        let certificate = document.getElementById('certificate');
-        certificate.classList = ""; // clear classes on each function call
+        let textSection = document.getElementById('textSection');
+        textSection.classList = ""; // clear classes on each function call
         switch (value) {
             case 'lecture':
-                certificate.classList.add('lecture');
+                document.getElementById('certDetails').innerText = "Being inconsiderate during a JavaScript lecture";
+                document.getElementById(`certDetails`).style.fontSize = '48px';
+                document.getElementById('explanation').innerText = `For failing to give due attention to the speaker during a JavaScript 
+                lecture on the day of ${getDate}`;
+                document.getElementById('explanation').style.fontSize = '20px';
+                textSection.classList.add('lecture');
                 break;
             case 'beans':
-                certificate.classList.add('beans');
+                document.getElementById('certDetails').innerText = "Bean Keeper";
+                document.getElementById(`certDetails`).style.fontSize = '50px';
+                textSection.classList.add('beans');
                 break;
             case 'quality':
-                certificate.classList.add('quality');
+                document.getElementById('certDetails').innerHTML = `<h2 id="certDetails">low q<span style="font-family: 'Roboto', sans-serif;">a</span><span style="font-family: 'Dancing Script', cursive;"></span>lity assurance</h2>`;
+                document.getElementById(`certDetails`).style.fontSize = '38px';
+                textSection.classList.add('quality');
                 break;
             case 'finishing':
-                certificate.classList.add('finishing');
+                document.getElementById('certDetails').innerText = `Not Finishing Your`;
+                document.getElementById(`certDetails`).style.fontSize = '50px';
+                textSection.classList.add('finishing');
                 break;
         }
     }
